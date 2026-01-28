d53dd1e6b1649fab8dd0b36a8080a553136bb622
Filip Hjerten <f.hjerten@gmail.com>
2026-01-28 18:13:11 +0100

    update page

--- data.py
+++ data.py
@@ -1,1 +1,19 @@
+import pandas as pd
+
+FILE_NAME = "players.csv"
+
+def load_players ():
+
+    players_table = pd.read_csv(FILE_NAME, sep=";")
+
+# Remove extra spaces
+
+    players_table.columns = [col.strip() for col in players_table.columns]
+
+    players_table["Name"] = players_table["Name"].str.strip()
+
+    return players_table
+
+print (load_players())
+
 
